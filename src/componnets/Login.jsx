fc6fa6dcc2904e208ade65af23706057b44a4693
Yehoshua Tarshish <a0526741852@gmail.com>
2023-06-30 11:17:56 +0300

    complited login/register without css. api is working well

--- src/componnets/Login.jsx
+++ src/componnets/Login.jsx
@@ -10,47 +10,51 @@ home page
 
 */
 
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import axios from "axios";
 import React, { useState } from "react";
 
 function Login() {
   const [email, setEmail] = useState([]);
   const [password, SetPassword] = useState([]);
-  const [firstName, setFirsName] = useState([]);
-  const [toggelRegLogin, setToggelRegLogin] = useState(true);
+  const [firstName, setFirstName] = useState([]);
+  const [lastName, setLastName] = useState([]);
+  const [username, setUserName] = useState([]);
 
-  let user = { email: email, password: password };
-  const alertIT = async () => {
-    const { data } = await axios.post(`/auth/login`, user);
-    setFirsName("hello " + data.user.firstName);
+  const [registerOrLogin, setRegisterOrLogin] = useState(null);
+
+  let user = { email, password, firstName, lastName, username };
+  const logRegFunction = async () => {
+    const data = await axios.post(
+      `http://localhost:4000/auth/${registerOrLogin}/`,
+      user
+    );
+    console.log(data);
+    // setFirstName("hello " + data.user.firstName);
   };
   return (
     <div>
-      <p>Login </p>
+      <div>
+        <h1 id="title">Todo App</h1>
+        <Button variant="contained" onClick={() => setRegisterOrLogin("login")}>
+          Login
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => setRegisterOrLogin("register")}
+        >
+          Register
+        </Button>
+      </div>
+
       <div>{firstName}</div>
-      <TextField
-        onChange={(e) => setEmail(e.target.value)}
-        id="username"
-        label="username"
-        variant="standard"
-      />
-      <TextField
-        onChange={(e) => SetPassword(e.target.value)}
-        id="password"
-        label="password"
-        variant="standard"
-        type="password"
-      />
-      <button onClick={() => alertIT()}>
-        {toggelRegLogin ? "login" : "regiser"}
-      </button>
-      {toggelRegLogin && (
+
+      {registerOrLogin && (
         <>
           <TextField
             onChange={(e) => setEmail(e.target.value)}
-            id="username"
-            label="username"
+            id="email"
+            label="Email"
             variant="standard"
           />
           <TextField
@@ -60,15 +64,36 @@ function Login() {
             variant="standard"
             type="password"
           />
-          <button onClick={() => alertIT()}>send</button>
         </>
       )}
-      <div>
-        not registerd yet?
-        <button onClick={() => setToggelRegLogin(!toggelRegLogin)}>
-          register
-        </button>
-      </div>
+      {registerOrLogin === "register" && (
+        <div className="registerFields">
+          <TextField
+            onChange={(e) => setFirstName(e.target.value)}
+            id="firstname"
+            label="Firs name"
+            variant="standard"
+          />
+          <TextField
+            onChange={(e) => setLastName(e.target.value)}
+            id="lastname"
+            label="Last name"
+            variant="standard"
+            type="text"
+          />
+          <TextField
+            onChange={(e) => setUserName(e.target.value)}
+            id="username"
+            label="Username"
+            variant="standard"
+          />
+        </div>
+      )}
+      {registerOrLogin && (
+        <Button onClick={() => logRegFunction()} variant="contained">
+          send
+        </Button>
+      )}
     </div>
   );
 }
