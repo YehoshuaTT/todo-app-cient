6d6b8583d6ebbd116597bac68fc4a9af502065d1
Yehoshua Tarshish <a0526741852@gmail.com>
2023-07-04 13:13:52 +0300

    added list, todo & category services classes

--- src/componnets/Main.jsx
+++ src/componnets/Main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Lists from "./Lists";
 
 function Main() {
   const navigate = useNavigate();
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -22,7 +22,9 @@ export default class HttpService {
       return false;
     }
   }
+}
 
+class ListService {
   static async listsIndex() {
     try {
       const { data } = await axios.get(`/lists`);
@@ -31,4 +33,161 @@ export default class HttpService {
       console.log(error);
     }
   }
+
+  static async listShow(listId) {
+    try {
+      const { data } = await axios.get(`/lists/${listId}`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async listCreate(listInfo) {
+    try {
+      const { data } = await axios.post(`/lists`, listInfo);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async listsUpdate(listId, update) {
+    try {
+      const { data } = await axios.put(`/lists/${listId}`, update);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async listDelete(listId) {
+    try {
+      const { data } = await axios.delete(`/lists/${listId}`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async listAddTodo(listId, todo) {
+    try {
+      const { data } = await axios.post(`/lists/${listId}/todos`, todo);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  static async listToggle(listId, todoId) {
+    try {
+      const { data } = await axios.put(`/lists/${listId}/todos${todoId}`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
+
+class TodoService {
+  static async todosIndex() {
+    try {
+      const { data } = await axios.get(`/todos`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async todoShow(todoId) {
+    try {
+      const { data } = await axios.get(`/todos/${todoId}`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async todoCreate(todoInfo) {
+    try {
+      const { data } = await axios.post(`/todos`, todoInfo);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async todoUpdate(todoId, update) {
+    try {
+      const { data } = await axios.put(`/todos/${todoId}`, update);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async todoDelete(todoId) {
+    try {
+      const { data } = await axios.delete(`/todos/${todoId}`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async todoToggle(todoId) {
+    try {
+      const { data } = await axios.put(`/todos/${todoId}/toggle`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+class CategoryService {
+  static async categoryIndex() {
+    try {
+      const { data } = await axios.get(`/categories`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async categoryShow(categoryId) {
+    try {
+      const { data } = await axios.get(`/categories/${categoryId}`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async categoryCreate(categoryInfo) {
+    try {
+      const { data } = await axios.post(`/categories`, categoryInfo);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async categoryUpdate(categoryId, update) {
+    try {
+      const { data } = await axios.put(`/categories/${categoryId}`, update);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async categoryDelete(categoryId) {
+    try {
+      const { data } = await axios.delete(`/categories/${categoryId}`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+module.exports = { ListService, CategoryService, TodoService };
